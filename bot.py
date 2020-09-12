df739733c895d5c50a54eb73ed7e4bf16db9143f
Mikey <mikey.rbx@gmail.com>
2020-09-12 17:35:08 +0100

    rules

--- bot.py
+++ bot.py
@@ -173,7 +173,30 @@ async def tag(ctx, argument):
     elif argument == "discord":
         embed = discord.Embed(title="Discord Invite", url="https://discord.gg/P24XMKP", description="This is the discord invite.", color=discord.Color.from_rgb(252, 206, 0))
         await ctx.send(embed=embed)
+    elif argument == "rules":
+        embed = discord.Embed(title="Kevinator Gang | Guidelines", description="1. You cannot ping staff members without permission in any situation.\n\n2. Respect people how you would like to be treated.\n\n3. In no circumstances can you disrespect anyone else in our community.\n\n4. Bullying is not tolerated, no matter whether it is a joke or not.\n\n5. Our Moderators know what they are doing and any arguments that arise from a moderator actions will be dealt with professionally. \n\n6. Failure to comply with Moderators can result in a severe punishment.\n\n7. People may not share links or files that may harm other users/yourself.\n\n8. Spamming chat will not be permitted.\n\n9. NSFW Language, Avatars or Nicknames will result in a punishment.\n\n10. Do not ask for any roles or permissions unless absolutely necessary.\n\n11. Terms and Conditions require to be followed by all members in our server.\n\n12. Content must be relevant to the channel you put it in.\n\n13. No Disrupting Stream.\n\n14. Punishments cannot be biased or unlawful, every punishment made by a Moderator must be reasoned.\n\n15. You shall not try to be annoying or hindering to a Moderator.\n\n16. You cannot bypass the rules in any way shape or form.", color=discord.Color.from_rgb(252, 206, 0))
+        await ctx.send(embed=embed)
     return
+
+@bot.command()
+@has_role("Bot Access")
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    kickEmbed = discord.Embed(title="Successfully Kicked.", description=member.display_name + " was kicked for: " + reason, color=discord.Color.from_rgb(0, 255, 0))
+    if reason == None:
+        kickEmbed.description = member.display_name + "was kicked successfully."
+    await ctx.send(embed=kickEmbed)
+
+@bot.command()
+@has_role("Bot Access")
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    banEmbed = discord.Embed(title="Successfully Banned.", description=member.display_name + " was banned for: " + reason, color=discord.Color.from_rgb(0, 255, 0))
+    if reason == None:
+        banEmbed.description = member.display_name + "was banned successfully."
+    await ctx.send(embed=banEmbed)
+
+
 
 @tag.error
 async def tag_error(ctx, error):
