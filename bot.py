7ec1b8459dc01f1699f22b2e13c37e7e41b4c5d0
Mikey <mikey.rbx@gmail.com>
2020-10-09 17:27:53 +0100

    sdsadajdaj

--- bot.py
+++ bot.py
@@ -228,7 +228,7 @@ async def help(ctx):
 @bot.command()
 async def update(ctx):
     updateEmbed = discord.Embed(title="Most recent update:", description="- Added update command\n- Renamed Branding to CORE\n- Added help command", thumbnail="https://cdn.discordapp.com/avatars/734495486723227760/dfc1991dc3ea8ec0f7d4ac7440e559c3.png?size=128", color=core_color)
-    await ctx.send(updateEmbed)
+    await ctx.send(embed=updateEmbed)
 
 @bot.command()
 @has_permissions(manage_channels=True)
