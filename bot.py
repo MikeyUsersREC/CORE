f56f3fe451bd1d6334ffc6e9604d3a40a7bdfe29
Mikey <mikey.rbx@gmail.com>
2020-10-10 15:48:19 +0100

    v2

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ async def setup(ctx):
     setupEmbed.set_thumbnail(url="https://cdn.discordapp.com/avatars/734495486723227760/dfc1991dc3ea8ec0f7d4ac7440e559c3.png?size=128")
     await ctx.send(embed=setupEmbed)
     setup_announcement_msg = bot.wait_for("message")
-    announcement_channel = "#" + setup_announcement_msg.content
+    announcement_channel = "#" + str(setup_announcement_msg.content)
     finishedEmbed = discord.embed(title="Setup Finished", description="The setup has completed successfully!", color=core_color)
     finishedEmbed.set_thumbnail(url="https://cdn.discordapp.com/avatars/734495486723227760/dfc1991dc3ea8ec0f7d4ac7440e559c3.png?size=128")
     await ctx.send(finishedEmbed)
