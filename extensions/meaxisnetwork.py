3487a007ceb4896dbb660d34bc3c0be6ef2b4858
Mikey <mikey.rbx@gmail.com>
2020-10-11 17:02:36 +0100

    v2

--- extensions/meaxisnetwork.py
+++ extensions/meaxisnetwork.py
@@ -7,7 +7,7 @@ core_color = discord.Color.from_rgb(30, 144, 255)
 async def test(ctx):
 	payload = {"discordid": ctx.author.id, "secret": "t6ovhm._7-ng9iry-1602428551-gy1pn37w.u06x8_q", "scope": "username"}
 	usernameRequest = requests.get("https://api.meaxisnetwork.net/v2/accounts/fromdiscord/", params=payload)
-	json = r.json()
+	json = usernameRequest.json()
 	username = json["message"]
 
 	payload = {"discordid": ctx.author.id, "secret": "t6ovhm._7-ng9iry-1602428551-gy1pn37w.u06x8_q", "scope": "avatarurl"}
