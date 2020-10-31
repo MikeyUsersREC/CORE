21aea218e0d06a4e314174a8ba88c8ef9e0e75fb
Mikey <mikey.rbx@gmail.com>
2020-10-31 16:17:23 +0000

    suppressed

--- bot.py
+++ bot.py
@@ -47,6 +47,76 @@ async def unload(ctx, extension):
     bot.unload_extension(f'extensions.{extension}')
 
 
+mn_color = discord.Color.from_rgb(35, 35, 35)
+meaxisnetwork_url = "https://meaxisnetwork.net/assets/images/square_logo.png"
+
+@bot.command()
+async def profile(ctx):
+	payload = {"discordid": ctx.author.id, "secret": "t6ovhm._7-ng9iry-1602428551-gy1pn37w.u06x8_q", "scope": "username"}
+	usernameRequest = requests.get("https://api.meaxisnetwork.net/v2/accounts/fromdiscord/", params=payload)
+	usernameJSON = usernameRequest.json()
+	username = usernameJSON["message"]
+
+	payload = {"discordid": ctx.author.id, "secret": "t6ovhm._7-ng9iry-1602428551-gy1pn37w.u06x8_q", "scope": "description"}
+	descriptionRequest = requests.get("https://api.meaxisnetwork.net/v2/accounts/fromdiscord/", params=payload)
+	descriptionJSON = descriptionRequest.json()
+	description = descriptionJSON["message"]
+	descriptionFixed = description.replace("\r", "")
+
+	payload = {"discordid": ctx.author.id, "secret": "t6ovhm._7-ng9iry-1602428551-gy1pn37w.u06x8_q", "scope": "profilepicture"}
+	avatarRequest = requests.get("https://api.meaxisnetwork.net/v2/accounts/fromdiscord/", params=payload)
+	avatarJSON = avatarRequest.json()
+	avatarURLSource = descriptionJSON["message"]
+	avatarURLString = str(avatarURLSource)
+	avatarURLFixed = avatarURLString.replace(r'\/','/')
+
+	payload = {"discordid": ctx.author.id, "secret": "t6ovhm._7-ng9iry-1602428551-gy1pn37w.u06x8_q", "scope": "id"}
+	accountIDRequest = requests.get("https://api.meaxisnetwork.net/v2/accounts/fromdiscord/", params=payload)
+	accountIDJSON = accountIDRequest.json()
+	AccountID = accountIDJSON["message"]
+
+	embed = discord.Embed(title=username, color=mn_color)
+	embed.add_field(name = "Description", value = description, inline = False)
+	embed.add_field(name = "Account ID", value = AccountID, inline = False)
+	embed.set_thumbnail(url=meaxisnetwork_url)
+	await ctx.send(embed=embed)
+
+@bot.command()
+async def funfact(ctx):
+	funfactRequest = requests.get("https://api.meaxisnetwork.net/v2/funfact/")
+	funfactJSON = funfactRequest.json()
+	funfact = funfactJSON["text"]
+	funfactID = funfactJSON["id"]
+	funfactAuthor = funfactJSON["author"]
+	
+	embed = discord.Embed(title=f"Funfact #{funfactID}", color=mn_color)
+	embed.add_field(name = "Funfact:", value = funfact, inline = False)
+	embed.add_field(name = "Author", value = funfactAuthor, inline = False)
+	embed.set_thumbnail(url=meaxisnetwork_url)
+	await ctx.send(embed=embed)
+
+@bot.command()
+async def leafy(ctx):
+	leafyRequest = requests.get("https://api.meaxisnetwork.net/v2/leafy/")
+	embed = discord.Embed(title=f"Leafy API Status", color=mn_color)
+	embed.add_field(name = "Status:", value = leafyRequest.status_code, inline = False)
+	embed.add_field(name = "Note:", value = "If the status is 200, then the leafy API is online.", inline = False)
+	embed.set_thumbnail(url=meaxisnetwork_url)
+	await ctx.send(embed=embed)
+	return
+
+@bot.command()
+async def finduser(ctx, username):
+	payload = {"username": username}
+	usernameRequest = requests.get("https://api.meaxisnetwork.net/v2/accounts/exists/", params=payload)
+	usernameJSON = usernameRequest.json()
+	usernameResult = usernameJSON["message"]
+	embed = discord.Embed(title="User Result", color=mn_color)
+	embed.add_field(name = "Username Entered:", value = username, inline = False)
+	embed.add_field(name = "Result:", value = usernameResult, inline = False)
+	embed.set_thumbnail(url=meaxisnetwork_url)
+	await ctx.send(embed=embed)
+
 @bot.command()
 async def run(ctx, *, cmd):
     if ctx.author.id == 635119023918415874:
